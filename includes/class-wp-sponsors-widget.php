f814ed659623e2e45bb85e35a714c346107ee888
janhenckens <janhenckens@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 16:39:29 +0000

    tagging 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-sponsors/tags/1.0.3@1109572 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-wp-sponsors-widget.php
+++ includes/class-wp-sponsors-widget.php
@@ -16,14 +16,20 @@ class sponsors_widget extends WP_Widget {
         'post_type'              => 'sponsor',
         'post_status'            => 'publish',
         'pagination'             => false,
-        'order'                  => 'ASC'
+        'order'                  => 'ASC',
+        'posts_per_page'        => '-1'
       );
-
+      $title = apply_filters('widget_title', $instance['title'] );
+      $before_title = "<h1 class='widget-title'>";
+      $after_title = "</h1>";
       // The Query
       $query = new WP_Query( $args );
       // The Output
       ?>
-      <div class="wp-sponsors widget">
+      <aside id="wp-sponsors" class="widget wp-sponsors">
+      <?php
+      if ( $title ) echo $before_title . $title . $after_title;
+      ?>
         <ul>
           <?php while ( $query->have_posts() ) : $query->the_post(); ?>
               <li class="sponsors-item">
@@ -35,17 +41,25 @@ class sponsors_widget extends WP_Widget {
               </li>
           <?php endwhile; wp_reset_postdata(); ?>
           </ul>
-        </div>
+        </aside>
       <?php
   }
   // Update the widget
   function update( $new_instance, $old_instance ) {
+
       $instance = $old_instance;
       $instance['check_images'] = $new_instance['check_images'];
+      $instance['title'] = strip_tags( $new_instance['title'] );
+
       return $instance;
   }
   // The Widget form
   function form($instance) {
+
+    //Set up some default widget settings.
+    $defaults = array( 'title' => __('Our sponsors', 'example_title'), 'check_images' => 'on' );
+    $instance = wp_parse_args( (array) $instance, $defaults );
+
     if(empty($instance)) {
       $key = array('check_images');
       $instance = array_fill_keys($key, 'on');
@@ -60,6 +74,10 @@ class sponsors_widget extends WP_Widget {
           />
         <label for="<?php echo $this->get_field_id('check_images'); ?>"><?php echo __( 'Show images', 'wp-sponsors' )?></label>
       </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title', 'example'); ?></label>
+        <input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+    </p>
     <?php
   }
 }
